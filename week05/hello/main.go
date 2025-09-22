c7d669d376e0e091de324c4c64f95b95fc0a73b8
nz663633 <hyunjisim3@gmail.com>
2025-09-22 19:33:13 +0900

    v0.2 hell/main.go & week05.txt update(2)

--- week05/hello/main.go
+++ week05/hello/main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	name := "Go developers"
-	fmt.Println("Git/Github for", name)
+	// name := "Go developers"
+	// fmt.Println("Git/Github for", name)
+	number := 7
+	fmt.Printf("Git/Github for %d\n", number)
 }
