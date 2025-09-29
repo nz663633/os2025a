de25594cb1117bd4be39816918ace09cbcc020a4
nz663633 <hyunjisim3@gmail.com>
2025-09-29 13:30:49 +0900

    v0.2 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	"math"
-	"strings"
+	"fmt"
+	"reflect"
 )
 
 func main() {
-	fmt.Println(math.Ceil(2.71))
-	fmt.Println(strings.Title("head first go"))
+	var f64 float64
+	var str string
+	var i32 int32
+	var b bool
+	fmt.Println(f64, reflect.TypeOf(f64))
+	fmt.Println(str, reflect.TypeOf(str))
+	fmt.Println(i32, reflect.TypeOf(i32))
+	fmt.Println(b, reflect.TypeOf(b))
 }
