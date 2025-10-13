1c8e61d948dcb7f03a15734b6a3486f07e42c28f
nz663633 <hyunjisim3@gmail.com>
2025-10-13 11:41:48 +0900

    loops/main.go v0.1 if

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -3,11 +3,41 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
+	// shadowing
+	// var fmt string = "inha"
+	// var int int = 7
+	// var k int = 11
+	// fmt.Println(int)
+
+	var now time.Time = time.Now()
+	var month time.Month = now.Month() // month := now.Month()
+	fmt.Println(month)
+
 	r := bufio.NewReader(os.Stdin)
-	i, _ := r.ReadString('\n') // ignore error
-	fmt.Println(i)
+	i, err := r.ReadString('\n')
+	// fmt.Println(err)
+	if err != nil {
+		log.Fatal(err) // report the error and exit program
+	}
+
+	i = strings.TrimSpace(i)
+	score, err := strconv.ParseFloat(i, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if score >= 60 {
+		fmt.Println("Pass")
+	} else {
+		fmt.Println("Fail")
+	}
+	fmt.Println(score)
 }
