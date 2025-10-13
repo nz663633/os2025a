a61745a08f38ca89db3e4204c0349d5205d957d3
nz663633 <hyunjisim3@gmail.com>
2025-10-13 10:49:15 +0900

    loops/main.go v0.1 time & strings package

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -1,4 +1,4 @@
-package main
+/* package main
 
 import (
 	"fmt"
@@ -6,10 +6,30 @@ import (
 )
 
 func main() {
+    // casting
 	var length float64 = 3.2
 	var width int = 2
 	fmt.Println("면적은", int(length)*width)
 	fmt.Println("길이 > 너비?", int(length) > width)
 	fmt.Println("형변환", reflect.TypeOf(int(length)))
 	fmt.Println("원본", reflect.TypeOf(length))
+}
+*/
+
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+func main() {
+	var now time.Time = time.Now()
+	var day int = now.Day()
+	fmt.Println(day)
+	univ := "Go Inha$!"
+	changer := strings.NewReplacer("$", "!")
+	changed := changer.Replace(univ)
+	fmt.Println(changed)
 }
