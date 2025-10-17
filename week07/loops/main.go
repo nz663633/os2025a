3f6b861f98ae3cd2de04c99fd568cb0bb5076850
nz663633 <hyunjisim3@gmail.com>
2025-10-17 11:15:56 +0900

    midterm summary(1)

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -1,43 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
-	"time"
+	"math/rand"
 )
 
 func main() {
-	// shadowing
-	// var fmt string = "inha"
-	// var int int = 7
-	// var k int = 11
-	// fmt.Println(int)
-
-	var now time.Time = time.Now()
-	var month time.Month = now.Month() // month := now.Month()
-	fmt.Println(month)
-
-	r := bufio.NewReader(os.Stdin)
-	i, err := r.ReadString('\n')
-	// fmt.Println(err)
-	if err != nil {
-		log.Fatal(err) // report the error and exit program
-	}
-
-	i = strings.TrimSpace(i)
-	score, err := strconv.ParseFloat(i, 64)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if score >= 60 {
-		fmt.Println("Pass")
-	} else {
-		fmt.Println("Fail")
-	}
-	fmt.Println(score)
+	// rand.Seed(55)
+	dice := rand.Intn(6) + 1 // 1 ~ 6
+	fmt.Println(dice)
 }
