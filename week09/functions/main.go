e5863e0e99fdda394a99d0aaea4cdb0db2c00d07
nz663633 <hyunjisim3@gmail.com>
2025-10-27 11:24:35 +0900

    functions/main.go v0.1 pointer

---
+++ week09/functions/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func swap(first int, second int) {
+	temp := 0
+	temp = first
+	first = second
+	second = temp
+	fmt.Println(first, second)
+}
+
+func main() {
+	var a, b int = 10, 20
+	fmt.Println(a, b)
+	swap(a, b)
+	fmt.Println(a, b)
+	// fmt.Printf("%.2f\n", math.Sqrt(-25.0))
+}
