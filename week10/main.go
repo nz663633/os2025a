f1496042ad20a42a7f1dbc7a0f6664d651876a42
nz663633 <hyunjisim3@gmail.com>
2025-11-03 10:40:30 +0900

    week10 package v0.2

--- week10/main.go
+++ week10/main.go
@@ -1,8 +1,27 @@
 package main
 
-import "greeting"
+// import "greeting"
+
+// func main() {
+// 	greeting.Hello()
+// 	greeting.Hi()
+// }
+
+import (
+	"fmt"
+	"keyboard"
+	"log"
+)
 
 func main() {
-	greeting.Hello()
-	greeting.Hi()
+	fmt.Print("점수 입력: ")
+	score, err := keyboard.GetFloat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if score >= 80 {
+		fmt.Printf("%.1f점은 합격!", score)
+	} else {
+		fmt.Printf("%.1f는 불합격...", score)
+	}
 }
