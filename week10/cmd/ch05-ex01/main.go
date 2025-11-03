c03cae3d7f5f06f5142137c676d17ca1110756d3
nz663633 <hyunjisim3@gmail.com>
2025-11-03 12:09:02 +0900

    week10 array v0.4

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -3,14 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	arrayBool := [3]bool{true, false, true}
-	arrayInt := [3]int{-9, 11, 7}
-
-	for i := 0; i < 3; i++ {
-		fmt.Println(i, arrayInt[i])
-		fmt.Println(i, arrayBool[i])
+	numbers := [3]int{-9, 11, 7}
+	for i, number := range numbers {
+		fmt.Println(i, number)
 	}
-	// fmt.Println(reflect.TypeOf(arrayBool))
-	// fmt.Printf("%#v\n", arrayBool)
-	// fmt.Printf("%#v\n", arrayInt)
 }
