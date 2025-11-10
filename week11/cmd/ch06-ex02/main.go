476ceba4a749c3c36d4b31a9e0d1a9ea10461de9
nz663633 <hyunjisim3@gmail.com>
2025-11-10 13:54:49 +0900

    week11 array v0.8

--- week11/cmd/ch06-ex02/main.go
+++ week11/cmd/ch06-ex02/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"log"
 	"os"
 	"strconv"
 )
 
-func GetFloats(fileName string) ([]float64, error) {
+func GetFloats(fileName string) ([3]float64, error) {
 	var numbers [3]float64
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -32,5 +34,14 @@ func GetFloats(fileName string) ([]float64, error) {
 }
 
 func main() {
-
+	weights, err := GetFloats("meatWeight.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	hap := 0.0
+	for i := 0; i < len(weights); i++ {
+		hap = hap + weights[i]
+	}
+	weeks := float64(len(weights))
+	fmt.Println("주차별 고기 소비량 평균: ", hap/weeks)
 }
