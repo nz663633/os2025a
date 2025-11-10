77200a5b92d336e656a535872638812976cc668a
nz663633 <hyunjisim3@gmail.com>
2025-11-10 11:29:56 +0900

    week11 slice v0.3

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	subjects := []string{"Go", "", "Python"} // initialized by slice literal
-
+	subjects := []string{"Go", "Javascript", "Python", "Linux"}
+	subjectsSlice := subjects[1:3] // slicing
 	for _, subject := range subjects {
 		fmt.Println(subject)
+	}
+	fmt.Println("===========")
+	for i := 0; i < len(subjectsSlice); i++ {
+		fmt.Println(subjectsSlice[i])
 	}
 }
