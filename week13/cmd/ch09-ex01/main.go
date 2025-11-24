82926794fa8d7e07ee5d8dfe487608767fb9c76c
nz663633 <hyunjisim3@gmail.com>
2025-11-24 10:59:07 +0900

    week13 types v0.1

---
+++ week13/cmd/ch09-ex01/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Meters float64
+type Kilometers float64
+type Miles float64
+
+func (l Meters) ToMiles() Miles {
+	return Miles(l * 0.621371)
+}
+func (m Kilometers) ToMiles() Miles {
+	return Miles(m * 0.000621371)
+}
+
+func main() {
+	kmph := Kilometers(150)
+	fmt.Printf("%0.2f kilo meter per hour equals %0.2f mile per hour\n", kmph, kmph.ToMiles())
+	meter := Kilometers(500)
+	fmt.Printf("%0.3f meter %0.3f miles\n", meter, meter.ToMiles())
+}
