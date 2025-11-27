6dc056ef97be8f39ec8ab3f94956c9252ebc06fd
nz663633 <hyunjisim3@gmail.com>
2025-11-27 19:08:18 +0900

    week13 encapsulation v0.2

--- week13/pkg/calender/date.go
+++ week13/pkg/calender/date.go
@@ -1,6 +1,9 @@
 package calender
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 type Date struct {
 	year  int
@@ -8,7 +11,12 @@ type Date struct {
 	day   int
 }
 
+func welcome() { // unexported
+	fmt.Println("환영합니다")
+}
+
 func (d *Date) Year() int {
+	welcome() // OK!
 	return d.year
 }
 
