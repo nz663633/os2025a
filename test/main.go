29ae258ae5a0e54415c62866bfca4dff50b4983f
nz663633 <hyunjisim3@gmail.com>
2025-11-30 15:39:19 +0900

    final exam review(5)

--- test/main.go
+++ test/main.go
@@ -1,40 +1,15 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
-)
+import "fmt"
 
-func GetFloat() (float64, error) { // 입력 받아서 float64로 변환하는 함수
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		return 0, err
-	}
-
-	input = strings.TrimSpace(input)
-	number, err := strconv.ParseFloat(input, 64)
-	if err != nil {
-		return 0, err
-	}
-	return number, nil
-}
 func main() {
-	fmt.Print("점수 입력: ")
-	score, err := GetFloat()
-	if err != nil {
-		log.Fatal(err)
+	numbers := [3]int{-9, 11, 7}
+	for i := 0; i < len(numbers); i++ {
+		fmt.Println(i, numbers[i])
 	}
 
-	status := ""
-	if score >= 60 {
-		status = "합격"
-	} else {
-		status = "불합격"
-	}
-	fmt.Printf("%.2f점은 %s\n", score, status)
+	// numbers := [3]int{-9, 11, 7}
+	// for i, number := range numbers {
+	// 	fmt.Println(i, number)
+	// }
 }
