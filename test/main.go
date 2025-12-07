3456cb403f344335dc7219698c5d07ed7166e1d7
nz663633 <hyunjisim3@gmail.com>
2025-12-07 22:44:01 +0900

    final exam review(8)

--- test/main.go
+++ test/main.go
@@ -1,26 +1,29 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	calendar "week13/pkg/calendar"
-)
+import "fmt"
+
+func abc(channel chan string) {
+	channel <- "내고향 스페셜\n"
+	channel <- "KBS 뉴스광장\n"
+	channel <- "인간극장\n"
+}
+
+func def(channel chan string) {
+	channel <- "건강의 재구성 설록(재)\n"
+	channel <- "오늘N\n"
+	channel <- "찾아가는 꾸러기 교실\n"
+}
 
 func main() {
-	// today := calendar.Date{year: 2025, month: 11, day: 24}
-	today := calendar.Date{}
-	err := today.SetYear(2025)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// err = today.SetMonth(15)
-	err = today.SetMonth(11)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = today.SetDay(24)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(today.year(), "년 ", today.Month(), "월 ", today.Day(), "일")
+	kbs := make(chan string)
+	mbc := make(chan string)
+	go abc(kbs)
+	go def(mbc)
+	fmt.Print(<-kbs)
+	fmt.Print(<-mbc)
+	fmt.Print(<-kbs)
+	fmt.Print(<-mbc)
+	fmt.Print(<-kbs)
+	fmt.Print(<-mbc)
+	fmt.Println()
 }
